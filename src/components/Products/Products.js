83a531093a4fc85f3c4960ae10d3da36f9fff870
Bharath siddoji <bharathsiddoji5@gmail.com>
2023-01-08 13:54:26 +0530

    made changes in product page

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import './products.css'
 
+import {useEffect,useState} from 'react'
 const Products = () => {
+  const [data,setData]= useState('')
+  useEffect(()=>{
+    async function fetchData(){
+      const response = await fetch('https://dummyjson.com/products')
+      const resData = await response.json()
+      setData(await resData)
+    }
+    fetchData()
+    console.log(data)
+  },[])
   return (
-    <div>Products</div>
+    <div className='product__page'>
+      
+    </div>
+    
   )
 }
 
