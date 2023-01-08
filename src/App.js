1cf065905b58a1fefa150a2b37be7413ffdc1cdb
Bharath siddoji <bharathsiddoji5@gmail.com>
2023-01-08 12:45:11 +0530

    added cart page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import  Navigation  from './components/navigation/Navigation'
 import Home from './components/homepage/Home';
+import Products from './components/Products/Products';
+import Cart from './components/cart/Cart';
 function App() {
   return (
     <>
@@ -9,8 +11,8 @@ function App() {
     <Navigation />
     <Routes>
       <Route path='/' element={<Home />}/>
-      <Route path='/' element={<Home />}/>
-      <Route path='/' element={<Home />}/>
+      <Route path='/products' element={<Products />}/>
+      <Route path='/cart' element={<Cart />}/>
     </Routes>
     </BrowserRouter>
     </>
--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -4,7 +4,16 @@ import "./nav.css";
 const Navigation = () => {
   return (
     <>
-      <Link>Home</Link>
+      <nav className="nav__bar">
+      <div className="logo">Logo</div>
+      <div>
+        <ul className="nav_links">
+          <Link to='/'>Home</Link>
+          <Link to='/products'>Products</Link>
+          <Link to='/cart'>Cart</Link>
+        </ul>
+      </div>
+      </nav>
     </>
   );
 };
