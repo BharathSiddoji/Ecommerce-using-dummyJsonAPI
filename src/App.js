1ef5ee792ed50e0f161c3dfde4b72c336905ada3
Bharath siddoji <bharathsiddoji5@gmail.com>
2023-01-08 12:39:38 +0530

    added components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-
+import './App.css'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import  Navigation  from './components/navigation/Navigation'
+import Home from './components/homepage/Home';
 function App() {
   return (
-    <div className="App">
-      hello
-    </div>
+    <>
+    <BrowserRouter >
+    <Navigation />
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/' element={<Home />}/>
+      <Route path='/' element={<Home />}/>
+    </Routes>
+    </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/components/navigation/Navigation.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+import React from "react";
+import "./nav.css";
+const Navigation = () => {
+  return (
+    <>
+      <Link>Home</Link>
+    </>
+  );
+};
+
+export default Navigation;
