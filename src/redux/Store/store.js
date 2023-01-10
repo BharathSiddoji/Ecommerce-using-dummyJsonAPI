079ce33d63954c4768d23ebff592630736a5853f
Bharath siddoji <bharathsiddoji5@gmail.com>
2023-01-10 22:33:26 +0530

    connected store to index.js

--- src/redux/Store/store.js
+++ src/redux/Store/store.js
@@ -1,1 +1,5 @@
 import {createStore,combineReducers} from 'redux';
+import {cartReducer} from '../reducer/reducer'
+
+const rootReducer=combineReducers({cartReducer})
+export const store = createStore(rootReducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
