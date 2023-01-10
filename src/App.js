d46661465346c2178bb233295b129fb625a7ddeb
Bharath siddoji <bharathsiddoji5@gmail.com>
2023-01-10 18:28:04 +0530

    configured The api to the products page

--- src/App.js
+++ src/App.js
@@ -4,14 +4,23 @@ import  Navigation  from './components/navigation/Navigation'
 import Home from './components/homepage/Home';
 import Products from './components/Products/Products';
 import Cart from './components/cart/Cart';
+import {useEffect, useState} from 'react'
+
+
 function App() {
+  const [data,setData]=useState([])
+    useEffect(()=>{
+    fetch('https://dummyjson.com/products')
+    .then(res => res.json())
+    .then(data=>setData(data.products) );
+  },[])
   return (
     <>
     <BrowserRouter >
     <Navigation />
     <Routes>
       <Route path='/' element={<Home />}/>
-      <Route path='/products' element={<Products />}/>
+      <Route path='/products' element={<Products data={data}/>}/>
       <Route path='/cart' element={<Cart />}/>
     </Routes>
     </BrowserRouter>
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,24 +1,35 @@
-import React from 'react'
-import './products.css'
+import React from "react";
+import "./products.css";
 
-import {useEffect,useState} from 'react'
-const Products = () => {
-  const [data,setData]= useState('')
-  useEffect(()=>{
-    async function fetchData(){
-      const response = await fetch('https://dummyjson.com/products')
-      const resData = await response.json()
-      setData(await resData)
-    }
-    fetchData()
-    console.log(data)
-  },[])
+const Products = (props) => {
+  const data = props.data;
   return (
-    <div className='product__page'>
-      
-    </div>
-    
-  )
-}
+    <>
+      <div className="product__container">
+        {data.length < 1 ? (
+          <li>Loading...</li>
+        ) : (
+          data.map((each) => (
+            <li key={each.id}>
+              <div className="item__card">
+                <div className="image">
+                  <img
+                    src={each.images[0]}
+                    alt="product_images"
+                    width={"200px"}
+                    height={"200px"}
+                  />
+                </div>
+                <div className="item__title">{each.title}</div>
+                <div className="item__price">${each.price}</div>
+                <button className="item__btn">Add to cart</button>
+              </div>
+            </li>
+          ))
+        )}
+      </div>
+    </>
+  );
+};
 
-export default Products
+export default Products;
